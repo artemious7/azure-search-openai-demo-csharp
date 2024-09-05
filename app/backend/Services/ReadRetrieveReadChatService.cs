c964509764744d23386f6eec2c628f8bacb54399
Artemious <16724889+artemious7@users.noreply.github.com>
2024-09-05 11:56:29 +0100

    refactor ReadRetrieveReadChatService

--- app/backend/Services/ReadRetrieveReadChatService.cs
+++ app/backend/Services/ReadRetrieveReadChatService.cs
@@ -79,71 +79,90 @@ public class ReadRetrieveReadChatService
             embeddings = (await embedding.GenerateEmbeddingAsync(question, cancellationToken: cancellationToken)).ToArray();
         }
 
+        string? query = await UseLLMToGetQueryIfRetrievalModeIsNotVector(overrides, chat, question, cancellationToken);
+        (string documentContents, SupportingContentRecord[] documentContentList) = await UseQueryToSearchRelatedDocs(overrides, embeddings, query, cancellationToken);
+        var images = await RetrieveImagesIfVisionServiceIsAvailable(overrides, question, query, default, cancellationToken);
+        (OpenAIPromptExecutionSettings promptExecutingSetting, string ans, string thoughts) = await PutTogetherRelatedDocsAndConversationHistoryToGenerateAnswer();
+        return await AddFollowUpQuestionsIfRequested();
+
         // step 1
         // use llm to get query if retrieval mode is not vector
-        string? query = null;
-        if (overrides?.RetrievalMode != RetrievalMode.Vector)
+        static async Task<string?> UseLLMToGetQueryIfRetrievalModeIsNotVector(RequestOverrides? overrides, IChatCompletionService chat, string question, CancellationToken cancellationToken)
         {
-            var getQueryChat = new ChatHistory(@"You are a helpful AI assistant, generate search query for followup question.
+            string? query = null;
+            if (overrides?.RetrievalMode != RetrievalMode.Vector)
+            {
+                var getQueryChat = new ChatHistory(@"You are a helpful AI assistant, generate search query for followup question.
 Make your respond simple and precise. Return the query only, do not return any other text.
 e.g.
 Northwind Health Plus AND standard plan.
 standard plan AND dental AND employee benefit.
 ");
 
-            getQueryChat.AddUserMessage(question);
-            var result = await chat.GetChatMessageContentAsync(
-                getQueryChat,
-                cancellationToken: cancellationToken);
+                getQueryChat.AddUserMessage(question);
+                var result = await chat.GetChatMessageContentAsync(
+                    getQueryChat,
+                    cancellationToken: cancellationToken);
 
-            query = result.Content ?? throw new InvalidOperationException("Failed to get search query");
+                query = result.Content ?? throw new InvalidOperationException("Failed to get search query");
+            }
+            return query;
         }
 
         // step 2
         // use query to search related docs
-        var documentContentList = await _searchClient.QueryDocumentsAsync(query, embeddings, overrides, cancellationToken);
+        async Task<(string documentContents, SupportingContentRecord[] documentContentList)> UseQueryToSearchRelatedDocs(RequestOverrides? overrides, float[]? embeddings, string? query, CancellationToken cancellationToken)
+        {
+            var documentContentList = await _searchClient.QueryDocumentsAsync(query, embeddings, overrides, cancellationToken);
 
-        string documentContents = string.Empty;
-        if (documentContentList.Length == 0)
-        {
-            documentContents = "no source available.";
-        }
-        else
-        {
-            documentContents = string.Join("\r", documentContentList.Select(x =>$"{x.Title}:{x.Content}"));
+            string documentContents = string.Empty;
+            if (documentContentList.Length == 0)
+            {
+                documentContents = "no source available.";
+            }
+            else
+            {
+                documentContents = string.Join("\r", documentContentList.Select(x => $"{x.Title}:{x.Content}"));
+            }
+            return (documentContents, documentContentList);
         }
 
         // step 2.5
         // retrieve images if _visionService is available
-        SupportingImageRecord[]? images = default;
-        if (_visionService is not null)
+        async Task<SupportingImageRecord[]?> RetrieveImagesIfVisionServiceIsAvailable(RequestOverrides? overrides, string question, string? query, SupportingImageRecord[]? images, CancellationToken cancellationToken)
         {
-            var queryEmbeddings = await _visionService.VectorizeTextAsync(query ?? question, cancellationToken);
-            images = await _searchClient.QueryImagesAsync(query, queryEmbeddings.vector, overrides, cancellationToken);
+            if (_visionService is not null)
+            {
+                var queryEmbeddings = await _visionService.VectorizeTextAsync(query ?? question, cancellationToken);
+                images = await _searchClient.QueryImagesAsync(query, queryEmbeddings.vector, overrides, cancellationToken);
+            }
+            return images;
         }
 
         // step 3
         // put together related docs and conversation history to generate answer
-        var answerChat = new ChatHistory(
-            "You are a system assistant who helps the company employees with their questions. Be brief in your answers");
-
-        // add chat history
-        foreach (var message in history)
+        async Task<(OpenAIPromptExecutionSettings promptExecutingSetting, string ans, string thoughts)> PutTogetherRelatedDocsAndConversationHistoryToGenerateAnswer()
         {
-            if (message.IsUser)
-            {
-                answerChat.AddUserMessage(message.Content);
-            }
-            else
-            {
-                answerChat.AddAssistantMessage(message.Content);
-            }
-        }
+            var answerChat = new ChatHistory(
+                "You are a system assistant who helps the company employees with their questions. Be brief in your answers");
 
-        
-        if (images != null)
-        {
-            var prompt = @$"## Source ##
+            // add chat history
+            foreach (var message in history)
+            {
+                if (message.IsUser)
+                {
+                    answerChat.AddUserMessage(message.Content);
+                }
+                else
+                {
+                    answerChat.AddAssistantMessage(message.Content);
+                }
+            }
+
+
+            if (images != null)
+            {
+                var prompt = @$"## Source ##
 {documentContents}
 ## End ##
 
@@ -151,22 +170,22 @@ Answer question based on available source and images.
 Your answer needs to be a json object with answer and thoughts field.
 Don't put your answer between ```json and ```, return the json string directly. e.g {{""answer"": ""I don't know"", ""thoughts"": ""I don't know""}}";
 
-            var tokenRequestContext = new TokenRequestContext(new[] { "https://storage.azure.com/.default" });
-            var sasToken = await (_tokenCredential?.GetTokenAsync(tokenRequestContext, cancellationToken) ?? throw new InvalidOperationException("Failed to get token"));
-            var sasTokenString = sasToken.Token;
-            var imageUrls = images.Select(x => $"{x.Url}?{sasTokenString}").ToArray();
-            var collection = new ChatMessageContentItemCollection();
-            collection.Add(new TextContent(prompt));
-            foreach (var imageUrl in imageUrls)
-            {
-                collection.Add(new ImageContent(new Uri(imageUrl)));
-            }
+                var tokenRequestContext = new TokenRequestContext(new[] { "https://storage.azure.com/.default" });
+                var sasToken = await (_tokenCredential?.GetTokenAsync(tokenRequestContext, cancellationToken) ?? throw new InvalidOperationException("Failed to get token"));
+                var sasTokenString = sasToken.Token;
+                var imageUrls = images.Select(x => $"{x.Url}?{sasTokenString}").ToArray();
+                var collection = new ChatMessageContentItemCollection();
+                collection.Add(new TextContent(prompt));
+                foreach (var imageUrl in imageUrls)
+                {
+                    collection.Add(new ImageContent(new Uri(imageUrl)));
+                }
 
-            answerChat.AddUserMessage(collection);
-        }
-        else
-        {
-            var prompt = @$" ## Source ##
+                answerChat.AddUserMessage(collection);
+            }
+            else
+            {
+                var prompt = @$" ## Source ##
 {documentContents}
 ## End ##
 
@@ -175,32 +194,36 @@ You answer needs to be a json object with the following format.
     ""answer"": // the answer to the question, add a source reference to the end of each sentence. e.g. Apple is a fruit [reference1.pdf][reference2.pdf]. If no source available, put the answer as I don't know.
     ""thoughts"": // brief thoughts on how you came up with the answer, e.g. what sources you used, what you thought about, etc.
 }}";
-            answerChat.AddUserMessage(prompt);
+                answerChat.AddUserMessage(prompt);
+            }
+
+            var promptExecutingSetting = new OpenAIPromptExecutionSettings
+            {
+                MaxTokens = 1024,
+                Temperature = overrides?.Temperature ?? 0.7,
+                StopSequences = [],
+            };
+
+            // get answer
+            var answer = await chat.GetChatMessageContentAsync(
+                           answerChat,
+                           promptExecutingSetting,
+                           cancellationToken: cancellationToken);
+            var answerJson = answer.Content ?? throw new InvalidOperationException("Failed to get search query");
+            var answerObject = JsonSerializer.Deserialize<JsonElement>(answerJson);
+            var ans = answerObject.GetProperty("answer").GetString() ?? throw new InvalidOperationException("Failed to get answer");
+            var thoughts = answerObject.GetProperty("thoughts").GetString() ?? throw new InvalidOperationException("Failed to get thoughts");
+            return (promptExecutingSetting, ans, thoughts);
         }
-
-        var promptExecutingSetting = new OpenAIPromptExecutionSettings
-        {
-            MaxTokens = 1024,
-            Temperature = overrides?.Temperature ?? 0.7,
-            StopSequences = [],
-        };
-
-        // get answer
-        var answer = await chat.GetChatMessageContentAsync(
-                       answerChat,
-                       promptExecutingSetting,
-                       cancellationToken: cancellationToken);
-        var answerJson = answer.Content ?? throw new InvalidOperationException("Failed to get search query");
-        var answerObject = JsonSerializer.Deserialize<JsonElement>(answerJson);
-        var ans = answerObject.GetProperty("answer").GetString() ?? throw new InvalidOperationException("Failed to get answer");
-        var thoughts = answerObject.GetProperty("thoughts").GetString() ?? throw new InvalidOperationException("Failed to get thoughts");
 
         // step 4
         // add follow up questions if requested
-        if (overrides?.SuggestFollowupQuestions is true)
+        async Task<ChatAppResponse> AddFollowUpQuestionsIfRequested()
         {
-            var followUpQuestionChat = new ChatHistory(@"You are a helpful AI assistant");
-            followUpQuestionChat.AddUserMessage($@"Generate three follow-up question based on the answer you just generated.
+            if (overrides?.SuggestFollowupQuestions is true)
+            {
+                var followUpQuestionChat = new ChatHistory(@"You are a helpful AI assistant");
+                followUpQuestionChat.AddUserMessage($@"Generate three follow-up question based on the answer you just generated.
 # Answer
 {ans}
 
@@ -213,35 +236,36 @@ e.g.
     ""What is the out-of-pocket maximum?""
 ]");
 
-            var followUpQuestions = await chat.GetChatMessageContentAsync(
-                followUpQuestionChat,
-                promptExecutingSetting,
-                cancellationToken: cancellationToken);
+                var followUpQuestions = await chat.GetChatMessageContentAsync(
+                    followUpQuestionChat,
+                    promptExecutingSetting,
+                    cancellationToken: cancellationToken);
 
-            var followUpQuestionsJson = followUpQuestions.Content ?? throw new InvalidOperationException("Failed to get search query");
-            var followUpQuestionsObject = JsonSerializer.Deserialize<JsonElement>(followUpQuestionsJson);
-            var followUpQuestionsList = followUpQuestionsObject.EnumerateArray().Select(x => x.GetString()!).ToList();
-            foreach (var followUpQuestion in followUpQuestionsList)
-            {
-                ans += $" <<{followUpQuestion}>> ";
+                var followUpQuestionsJson = followUpQuestions.Content ?? throw new InvalidOperationException("Failed to get search query");
+                var followUpQuestionsObject = JsonSerializer.Deserialize<JsonElement>(followUpQuestionsJson);
+                var followUpQuestionsList = followUpQuestionsObject.EnumerateArray().Select(x => x.GetString()!).ToList();
+                foreach (var followUpQuestion in followUpQuestionsList)
+                {
+                    ans += $" <<{followUpQuestion}>> ";
+                }
+
+                followUpQuestionList = followUpQuestionsList.ToArray();
             }
 
-            followUpQuestionList = followUpQuestionsList.ToArray();
+            var responseMessage = new ResponseMessage("assistant", ans);
+            var responseContext = new ResponseContext(
+                DataPointsContent: documentContentList.Select(x => new SupportingContentRecord(x.Title, x.Content)).ToArray(),
+                DataPointsImages: images?.Select(x => new SupportingImageRecord(x.Title, x.Url)).ToArray(),
+                FollowupQuestions: followUpQuestionList ?? Array.Empty<string>(),
+                Thoughts: new[] { new Thoughts("Thoughts", thoughts) });
+
+            var choice = new ResponseChoice(
+                Index: 0,
+                Message: responseMessage,
+                Context: responseContext,
+                CitationBaseUrl: _configuration.ToCitationBaseUrl());
+
+            return new ChatAppResponse(new[] { choice });
         }
-
-        var responseMessage = new ResponseMessage("assistant", ans);
-        var responseContext = new ResponseContext(
-            DataPointsContent: documentContentList.Select(x => new SupportingContentRecord(x.Title, x.Content)).ToArray(),
-            DataPointsImages: images?.Select(x => new SupportingImageRecord(x.Title, x.Url)).ToArray(),
-            FollowupQuestions: followUpQuestionList ?? Array.Empty<string>(),
-            Thoughts: new[] { new Thoughts("Thoughts", thoughts) });
-
-        var choice = new ResponseChoice(
-            Index: 0,
-            Message: responseMessage,
-            Context: responseContext,
-            CitationBaseUrl: _configuration.ToCitationBaseUrl());
-
-        return new ChatAppResponse(new[] { choice });
     }
 }
